ede96df647943fab6fd2046d1d50d9ba39dd6346
Maiia Bocharova <71256026+maiiabocharova@users.noreply.github.com>
2021-02-12 12:25:50 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,9 +1,6 @@
 from core import create_app
-#C:\Users\Омут\PycharmProjects\app
-#C:\Program Files\PostgreSQL\13\data
 app = create_app()
 
 if __name__ == "__main__":
-    #app.run(debug=True, host='0.0.0.0', port=8000)
     app.run(debug=True, host='0.0.0.0', port=8000)
 
